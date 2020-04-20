c92bb843ac30ae5678d59dd3e826ff3e2f98bc1d
pawel <pkoselski@gmail.com>
2020-04-20 14:31:14 +0200

    Added new fields to Transactions + DB migration

--- Models/Transactions.cs
+++ Models/Transactions.cs
@@ -1,8 +1,20 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class Transactions
 {
     public int Id { get; set; }
     public DateTime Date { get; set; }
     public string Stock { get; set; }
+    public string Market { get; set; }
+    public string Operation { get; set; }
+    public decimal Quantity { get; set; }
+    public decimal Price { get; set; }
+    public string PriceCurrencySymbol { get; set; }
+    public decimal Amount { get; set; }
+    public string AmountCurrencySymbol { get; set; }
+    public decimal Commision { get; set; }
+    
+    [Column(TypeName = "jsonb")]
+    public string Jsondata { get; set; }
 }
