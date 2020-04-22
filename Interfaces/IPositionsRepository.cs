174e81571764be228cca2b52084fa0680e3e2010
Pawel Koselski <pawel.koselski@aptitudesoftware.com>
2020-04-22 07:58:17 +0200

    PositionsRepository interface added

---
+++ Interfaces/IPositionsRepository.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using transactions_api.Models;
+
+namespace transactions_api.Interfaces
+{
+    public interface IPositionsRepository
+    {
+        Task<List<TransactionsGroup>> ListByDateAsync(DateTime date);
+        Task<List<TransactionsGroup>> ListByTickerAsync(DateTime date, string ticker);
+    }
+}
