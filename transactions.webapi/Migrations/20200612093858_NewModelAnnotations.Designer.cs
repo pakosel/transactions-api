eae00bb03c0185b321c19808c8f77f00b8827a15
pawel <pkoselski@gmail.com>
2020-06-12 11:41:19 +0200

    EF migrations added

--- transactions.webapi/Migrations/20200612093858_NewModelAnnotations.Designer.cs
+++ transactions.webapi/Migrations/20200612093858_NewModelAnnotations.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using transactions_api.Models;
@@ -9,9 +10,10 @@ using transactions_api.Models;
 namespace transactions_api.Migrations
 {
     [DbContext(typeof(MyWebApiContext))]
-    partial class MyWebApiContextModelSnapshot : ModelSnapshot
+    [Migration("20200612093858_NewModelAnnotations")]
+    partial class NewModelAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ transactions.webapi/Migrations/20200612093858_NewModelAnnotations.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace transactions_api.Migrations
+{
+    public partial class NewModelAnnotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Stock",
+                table: "Transactions",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Operation",
+                table: "Transactions",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Stock",
+                table: "Transactions",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Operation",
+                table: "Transactions",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
