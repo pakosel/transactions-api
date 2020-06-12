c062b5fa36f185dfa3a9496a3cc16b62cc13bd61
Pawel Koselski <pawel753@gmail.com>
2020-06-12 10:58:16 +0200

    Model annotations

--- transactions.webapi/Models/Transaction.cs
+++ transactions.webapi/Models/Transaction.cs
@@ -1,18 +1,27 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace transactions_api.Models
 {
     public class Transaction
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+        [Required]
         public DateTime Date { get; set; }
+        [Required]
         public string Stock { get; set; }
         public string Market { get; set; }
+        [Required]
         public string Operation { get; set; }
+        [Required]
         public decimal Quantity { get; set; }
+        [Required]
         public decimal Price { get; set; }
         public string PriceCurrencySymbol { get; set; }
+        [Required]
         public decimal Amount { get; set; }
         public string AmountCurrencySymbol { get; set; }
         public Nullable<decimal> Commision { get; set; }
